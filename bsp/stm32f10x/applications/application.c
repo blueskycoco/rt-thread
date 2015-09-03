e01c05343089292cc65baec5291ee63f62934846
U-XBL9VNFWMJBYVNH\Administrator <Administrator@XBL9VNFWMJBYVNH.(none)>
2015-09-03 22:10:05 +0800

    wifi to server http mode ok

--- bsp/stm32f10x/applications/application.c
+++ bsp/stm32f10x/applications/application.c
@@ -42,10 +42,10 @@
 #endif
 
 #include "led.h"
-static rt_uint8_t lcd_stack[ 512 ];
+static rt_uint8_t lcd_stack[ 1024 ];
 static struct rt_thread lcd_thread;
-struct rt_semaphore co2_rx_sem;
-rt_device_t dev_co2;
+struct rt_semaphore co2_rx_sem,wifi_rx_sem;
+rt_device_t dev_co2,dev_wifi;
 int data_co2=0;
 ALIGN(RT_ALIGN_SIZE)
 static rt_uint8_t led_stack[ 512 ];
@@ -85,11 +85,12 @@ static rt_err_t co2_rx_ind(rt_device_t dev, rt_size_t size)
 }
 void common_w_usb(void* parameter)
 {	
+	
+	char *ptr=rt_malloc(128);			
 	while(1)	
 	{		
 		if (rt_sem_take(&(co2_rx_sem), RT_WAITING_FOREVER) != RT_EOK) continue;		
-		char *ptr=rt_malloc(128);	
-		int len=rt_device_read(dev_co2, 0, ptr, 128);	
+		int len=rt_device_read(dev_co2, 0, ptr, 128);
 		if(len>0)	
 		{	
 			int i;		
@@ -100,8 +101,63 @@ void common_w_usb(void* parameter)
 			}	
 			data_co2=ptr[2]*256+ptr[3];
 			rt_kprintf(" %d\n",data_co2);
+			//rt_free(ptr);
+		}		
+	}	
+}
+static rt_err_t wifi_rx_ind(rt_device_t dev, rt_size_t size)
+{
+	/* release semaphore to let finsh thread rx data */	
+	//DBG("dev %d common_rx_ind %d\r\n",which_common_dev(common_dev,dev),size);    
+	rt_sem_release(&(wifi_rx_sem));    
+	return RT_EOK;
+}
+void wifi_rcv(void* parameter)
+{	
+	int len=0,i=0,state=0;
+	unsigned char ch;	
+	char *ptr=rt_malloc(256);	
+
+	while(1)	
+	{		
+		if (rt_sem_take(&(wifi_rx_sem), RT_WAITING_FOREVER) != RT_EOK) continue;	
+		#if 0
+		while(1)
+		{
+			if(rt_device_read(dev_wifi, 0, &ch, 1)==1)
+			{
+				ptr[i]=ch;
+				rt_kprintf("%c",ptr[i]);
+				if(ptr[i]=='+')
+					state=1;
+				else if(ptr[i]=='o' && state==1)
+					state=2;
+				else if(ptr[i]=='k' && state==2)
+				{
+					state=0;
+					break;
+				}
+				i++;
+			}
+		}
+		len=i;
+		#else
+		len=rt_device_read(dev_wifi, 0, ptr+i, 128);
+		i+=len;
+		#endif
+		if(strstr(ptr,"+ok")!=RT_NULL)	
+		{	
+			int j;		
+			rt_kprintf("Get from Wifi:\n");
+			for(j=0;j<i;j++)		
+			{		
+				rt_kprintf("%c",ptr[j]);
+			}	
+			rt_kprintf("\n");
+			memset(ptr,0,256);
+			i=0;
+			//rt_free(ptr);	
 		}	
-		rt_free(ptr);	
 	}	
 }
 
@@ -110,6 +166,9 @@ static void lcd_thread_entry(void* parameter)
 	int val1=0,val2=1,val3=2,val4=3,val5=4,val6=5;
 	uint8_t bat1=20,bat2=0;
 	unsigned char read_co2[10]={0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+	unsigned char switch_at[4]={'+','+','+'};
+	unsigned char done='a';
+	unsigned char httpd_send[64]={0};//"AT+HTTPDT\r\n";
 	u8 str[100];	/*将数字信息填充到str里*/
 	sprintf(str,"%d%d%d%d%d.%d",val1,val2,val3,val4,val5,val6);
 	/*初始化ssd1306*/    
@@ -132,6 +191,30 @@ static void lcd_thread_entry(void* parameter)
 		rt_device_set_rx_indicate(dev_co2, co2_rx_ind);
 		rt_thread_startup(rt_thread_create("thread_co2",common_w_usb, 0,512, 20, 10));
 		rt_device_write(dev_co2, 0, (void *)read_co2, 9);
+	}	
+	dev_wifi=rt_device_find("uart2");
+	if (rt_device_open(dev_wifi, RT_DEVICE_OFLAG_RDWR | RT_DEVICE_FLAG_INT_RX) == RT_EOK)			
+	{		
+		unsigned char httpd_url[64]={0};//"AT+HTTPURL=http://101.200.182.92,8080\r\n";
+		unsigned char httpd_mode[64]={0};//"AT+HTTPTP=GET\r\n";	
+		unsigned char httpd_local[64]={0};//"AT+HTTPPH=/mango/checkDataYes\r\n";
+		strcpy(httpd_url,"AT+HTTPURL=http://101.200.182.92,8080\r\n");
+		strcpy(httpd_mode,"AT+HTTPTP=GET\r\n");
+		strcpy(httpd_local,"AT+HTTPPH=/mango/checkDataYes\r\n");
+		strcpy(httpd_send,"AT+HTTPDT\r\n");
+		rt_sem_init(&(wifi_rx_sem), "wifi_rx", 0, 0);
+		rt_device_set_rx_indicate(dev_wifi, wifi_rx_ind);
+		rt_thread_startup(rt_thread_create("thread_wifi",wifi_rcv, 0,512, 20, 10));
+		rt_device_write(dev_wifi, 0, (void *)switch_at, 3);
+		rt_thread_delay(10);
+		rt_device_write(dev_wifi, 0, (void *)&done, 1);
+		rt_thread_delay(30);
+		rt_device_write(dev_wifi, 0, (void *)httpd_url, strlen(httpd_url));
+		rt_thread_delay(30);
+		rt_device_write(dev_wifi, 0, (void *)httpd_mode, strlen(httpd_mode));
+		rt_thread_delay(30);
+		rt_device_write(dev_wifi, 0, (void *)httpd_local, strlen(httpd_local));
+		rt_thread_delay(30);
 	}
 	while(1){
 		val1++;
@@ -143,9 +226,10 @@ static void lcd_thread_entry(void* parameter)
 		bat1++;
 		bat2++;
 		//rt_kprintf("cur str is %s\n",str);
-		rt_thread_delay(30);
+		rt_thread_delay(500);
 		rt_device_write(dev_co2, 0, (void *)read_co2, 9);
 		sprintf(str,"%07d",data_co2);
+		rt_device_write(dev_wifi, 0, (void *)httpd_send, strlen(httpd_send));
 		draw(bat1,bat2,str);
 		display();
 		if(val1==9)
