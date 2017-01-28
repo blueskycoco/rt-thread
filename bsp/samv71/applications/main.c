ed4903772fd0b82ced99d288f469d500d7c518a2
U-F-7F8724A5CE8D4\bbstr <bbstr@f-7f8724a5ce8d4.(none)>
2017-01-28 22:20:36 +0800

    fix msh rt_malloc issue

--- bsp/samv71/applications/main.c
+++ bsp/samv71/applications/main.c
@@ -34,12 +34,32 @@
 #include "drv_qspi.h"
 #include "drv_sdio.h"
 #endif 
+static struct rt_semaphore rx_sem;
+rt_device_t dev_usart1 = RT_NULL;
+
+static rt_err_t rx_ind(rt_device_t dev, rt_size_t size)
+{
+    rt_sem_release(&rx_sem);
+    return RT_EOK;
+}
+static void usart1_rx(void* parameter)
+{
+	int len = 0;
+	rt_uint8_t buf[256] = {0};
+	while (1)
+	{	
+		if (rt_sem_take(&rx_sem, RT_WAITING_FOREVER) != RT_EOK) continue;
+		len=rt_device_read(dev_usart1, 0, buf, 256);
+		buf[len]='\0';
+		rt_kprintf("%s", buf);
+	}
+}
 void mnt_init(void)
 {
 
     if (RT_EOK != rt_hw_sdio_init())
 		return ;
-    rt_thread_delay(RT_TICK_PER_SECOND * 1);
+    //rt_thread_delay(RT_TICK_PER_SECOND * 1);
 
     /* mount sd card fat partition 1 as root directory */
     if (dfs_mount("sd0", "/sd", "elm", 0, 0) == 0)
@@ -66,6 +86,21 @@ void mnt_init(void)
 int main(void)
 {
 	/* put user application code here */
+	dev_usart1 = rt_device_find("uart1");
+
+	if (dev_usart1 == RT_NULL) {
+		rt_kprintf("can not find usart1 \n");
+		return 0;
+	}
+	if (rt_device_open(dev_usart1, 
+		RT_DEVICE_OFLAG_RDWR | RT_DEVICE_FLAG_INT_RX 
+		) == RT_EOK)
+	{
+		rt_sem_init(&rx_sem, "usart1_sem", 0, 0);
+		rt_device_set_rx_indicate(dev_usart1, rx_ind);
+		rt_thread_startup(rt_thread_create("usart1_rx",
+			usart1_rx, RT_NULL,2048, 20, 10));
+	}
 #ifdef RT_USING_DFS
 	rt_hw_spi_init();	
     rt_sfud_flash_probe("flash", "spi10");	
