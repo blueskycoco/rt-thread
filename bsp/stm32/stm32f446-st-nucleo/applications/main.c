55ad9dd27ed73488d714ea2e003f8ee7850c765a
your <your@example.com>
2020-08-06 21:58:01 +0800

    try add heart

--- bsp/stm32/stm32f446-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f446-st-nucleo/applications/main.c
@@ -13,12 +13,14 @@
 #include <board.h>
 #include <ctype.h>
 #include "icm20603.h"
-
+#include "crc.h"
 static struct rt_thread usb_thread;
 ALIGN(RT_ALIGN_SIZE)
-static char usb_thread_stack[512];
+static char usb_thread_stack[1024];
 static struct rt_semaphore tx_sem_complete;
-
+rt_device_t ts_device;
+rt_uint8_t hid_rcv[64] = {0};
+rt_size_t g_hid_size;
 rt_sem_t        isr_sem;
 /* defined the LED2 pin: PB7 */
 #define LED2_PIN    GET_PIN(B, 7)
@@ -61,6 +63,73 @@ static rt_err_t event_hid_in(rt_device_t dev, void *buffer)
     return RT_EOK;
 }
 
+static rt_uint32_t read_ts()
+{
+    rt_hwtimerval_t val,val1;
+    rt_device_control(ts_device, HWTIMER_CTRL_STOP, RT_NULL);
+    rt_device_read(ts_device, 0, &val, sizeof(val));
+    val1.sec = 10;
+    val1.usec = 0;
+    if (rt_device_write(ts_device, 0, &val1, sizeof(val1)) != sizeof(val1))
+    	    rt_kprintf("set timer failed\n");
+    return (val.sec*1000000+val.usec);
+}
+static void handle_heart(rt_device_t device, rt_uint8_t *data, rt_size_t size)
+{
+	rt_uint8_t tx[64] = {0};
+	rt_uint32_t t2 = 0, t3 = 0;
+	rt_uint8_t ts2_ascii[8], ts3_ascii[8], crc_ascii[8];
+	t2 = read_ts();
+	//rt_kprintf("=> %d\n", t2);
+	if (data[0] == ':' &&
+		data[1] == '@' &&
+		data[2] == ':' &&
+		data[3] == 'K') {
+		/* heart T1, T2, T3, T4 */
+		tx[0] = 0x02;
+		tx[1] = 0x3a;
+		tx[2] = 0x41;
+		tx[3] = 0x3a;
+		tx[4] = 0x4b;
+		tx[5] = 0x3a;
+		rt_memcpy(tx+6, data+5, 16);
+		tx[22] = 0x26;
+		ByteToHexStr(t2, ts2_ascii);
+		rt_memcpy(tx+23, ts2_ascii, 8);
+		tx[31] = 0x3a;
+		t3 = read_ts();
+		ByteToHexStr(t3, ts3_ascii);
+		rt_memcpy(tx+32, ts3_ascii, 8);
+		tx[40] = 0x3a;
+		uint32_t crc = heart_cmd_crc(tx, 41);
+		ByteToHexStr(crc,crc_ascii);
+		if (crc_ascii[0] == '0' &&
+			crc_ascii[1] == '0' &&
+			crc_ascii[2] == '0') {
+			crc_ascii[0] = ' ';
+			crc_ascii[1] = ' ';
+			crc_ascii[2] = ' ';
+		} else if (crc_ascii[0] == '0' &&
+			   crc_ascii[1] == '0'	) {
+			crc_ascii[0] = ' ';
+			crc_ascii[1] = ' ';
+		} else if (crc_ascii[0] == '0')
+			crc_ascii[0] = ' ';
+		rt_memcpy(tx+41, crc_ascii, 8);
+		tx[49] = 0x3a;
+		tx[50] = 0x03;
+        	if (rt_device_write(device, /*HID_REPORT_ID_GENERAL*/0x02, tx+1, 50)
+        			== 50)
+		{
+            		//rt_sem_take(&tx_sem_complete, RT_WAITING_FOREVER);
+			rt_kprintf("heart out ok, t2 %d, t3 %d\n", t2, t3);
+		}
+		else
+			rt_kprintf("heart out failed\n");
+
+	}
+
+}
 static void usb_thread_entry(void *parameter)
 {
     int8_t i8MouseTable[] = { -16, -16, -16, 0, 16, 16, 16, 0};
@@ -72,13 +141,16 @@ static void usb_thread_entry(void *parameter)
 
     rt_sem_init(&tx_sem_complete, "tx_complete_sem_hid", 1, RT_IPC_FLAG_FIFO);
 
-    rt_device_set_tx_complete(device, event_hid_in);
+    //rt_device_set_tx_complete(device, event_hid_in);
 
     rt_kprintf("Ready.\n");
 
     while (1)
     {
-        u8MouseMode ^= 1;
+        rt_sem_take(&tx_sem_complete, RT_WAITING_FOREVER);
+    	handle_heart(device, hid_rcv, g_hid_size);
+#if 0
+	    u8MouseMode ^= 1;
         if (u8MouseMode)
         {
             if (u8MoveLen > 14)
@@ -109,31 +181,34 @@ static void usb_thread_entry(void *parameter)
             /* Wait it done. */
             rt_sem_take(&tx_sem_complete, RT_WAITING_FOREVER);
         }
+#endif
 
     } // while(1)
 }
 static void dump_data(rt_uint8_t *data, rt_size_t size)
 {
     rt_size_t i;
+    rt_uint8_t *ptr = data;
     for (i = 0; i < size; i++)
     {
-    	if (isascii(*data))
-        rt_kprintf("%c", *data++);
-	else
-        rt_kprintf("%02x ", *data++);
-        /*if ((i + 1) % 8 == 0)
-        {
-            rt_kprintf("\n");
-        }else if ((i + 1) % 4 == 0){
-            rt_kprintf(" ");
-        }*/
+        rt_kprintf("%c", *ptr++);
     }
     rt_kprintf("\n");
+
+	if (data[0] == ':' &&
+		data[1] == '@' &&
+		data[2] == ':' &&
+		data[3] == 'K') {
+		rt_memset(hid_rcv, 0, 64);
+		rt_memcpy(hid_rcv, data, size);
+		g_hid_size = size;
+    		rt_sem_release(&tx_sem_complete);
+	}
 }
 static void dump_report(struct hid_report * report)
 {
-    rt_kprintf("\nHID Recived:");
-    rt_kprintf("\nReport ID %02x \n", report->report_id);
+    //rt_kprintf("\nHID Recived:");
+    //rt_kprintf("\nReport ID %02x \n", report->report_id);
     dump_data(report->report,report->size);
 }
 void HID_Report_Received(hid_report_t report)
@@ -143,11 +218,14 @@ void HID_Report_Received(hid_report_t report)
 static int generic_hid_init(void)
 {
     int err = 0;
-    rt_device_t device = rt_device_find("hidd");
+	rt_device_t hid_device;
+    rt_hwtimer_mode_t mode;
+    rt_hwtimerval_t val;
+    hid_device = rt_device_find("hidd");
 
-    RT_ASSERT(device != RT_NULL);
+    RT_ASSERT(hid_device != RT_NULL);
 
-    err = rt_device_open(device, RT_DEVICE_FLAG_RDWR);
+    err = rt_device_open(hid_device, RT_DEVICE_FLAG_RDWR);
 
     if (err != RT_EOK)
     {
@@ -155,14 +233,32 @@ static int generic_hid_init(void)
         return -1;
     }
 
+    if ((ts_device = rt_device_find("timer3")) == RT_NULL)
+    {
+        rt_kprintf("No Device: timer3\n");
+        return -1;
+    }
+
+    if (rt_device_open(ts_device, RT_DEVICE_OFLAG_RDWR) != RT_EOK)
+    {
+        rt_kprintf("Open timer3 Fail\n");
+        return -1;
+    }
+    mode = HWTIMER_MODE_PERIOD;
+    err = rt_device_control(ts_device, HWTIMER_CTRL_MODE_SET, &mode);
+    val.sec = 10;
+    val.usec = 0;
+    rt_kprintf("SetTime: Sec %d, Usec %d\n", val.sec, val.usec);
+    if (rt_device_write(ts_device, 0, &val, sizeof(val)) != sizeof(val))
+    	    rt_kprintf("set timer failed\n");
     rt_thread_init(&usb_thread,
                    "hidd_app",
-                   usb_thread_entry, device,
+                   usb_thread_entry, hid_device,
                    usb_thread_stack, sizeof(usb_thread_stack),
                    10, 20);
 
-    //rt_thread_startup(&usb_thread);
-
+    rt_thread_startup(&usb_thread);
+    
     return 0;
 }
 int main(void)
