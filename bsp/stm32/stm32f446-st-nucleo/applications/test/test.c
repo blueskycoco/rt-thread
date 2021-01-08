5a5ff7d9980dd4bbdf80d02794991e40701bb888
fmin-nreal <fmin@nreal.ai>
2021-01-08 14:00:43 +0800

    print two imu data in single line

--- bsp/stm32/stm32f446-st-nucleo/applications/test/test.c
+++ bsp/stm32/stm32f446-st-nucleo/applications/test/test.c
@@ -23,8 +23,8 @@ int main(int argc, void* argv[])
 	int send = 0, rcv_len = 0;
 	uint8_t cmd[64] = {0x55, 0x12, 0x33};
 	uint8_t rsp[64];
-	int16_t acc[3], gyro[3];
-	float accf[3], gyrof[3];
+	int16_t acc[6], gyro[6];
+	float accf[6], gyrof[6];
 
 	if (argc >= 2)
 		send = 1;
@@ -51,20 +51,22 @@ int main(int argc, void* argv[])
 			gyro[0] = (int16_t)((rsp[13] << 24) | (rsp[14] << 16) | (rsp[15] << 8) | (rsp[16] << 0));
 			gyro[1] = (int16_t)((rsp[17] << 24) | (rsp[18] << 16) | (rsp[19] << 8) | (rsp[20] << 0));
 			gyro[2] = (int16_t)((rsp[21] << 24) | (rsp[22] << 16) | (rsp[23] << 8) | (rsp[24] << 0));
+			//printf("icm20602 %10f, %10f, %10f, %10f, %10f, %10f\r\n",
+			//		accf[0], accf[1], accf[2],
+			//		gyrof[0], gyrof[1], gyrof[2]);
+			acc[3] = (int16_t)((rsp[25] << 24) | (rsp[26] << 16) | (rsp[27] << 8) | (rsp[28] << 0));
+			acc[4] = (int16_t)((rsp[29] << 24) | (rsp[30] << 16) | (rsp[31] << 8) | (rsp[32] << 0));
+			acc[5] = (int16_t)((rsp[33] << 24) | (rsp[34] << 16) | (rsp[35] << 8) | (rsp[36] << 0));
+			gyro[3] = (int16_t)((rsp[37] << 24) | (rsp[38] << 16) | (rsp[39] << 8) | (rsp[40] << 0));
+			gyro[4] = (int16_t)((rsp[41] << 24) | (rsp[42] << 16) | (rsp[43] << 8) | (rsp[44] << 0));
+			gyro[5] = (int16_t)((rsp[45] << 24) | (rsp[46] << 16) | (rsp[47] << 8) | (rsp[48] << 0));
 			convert2float(acc, gyro, accf, gyrof);
-			printf("icm20602 %10f, %10f, %10f, %10f, %10f, %10f\r\n",
+			convert2float(acc+3, gyro+3, accf+3, gyrof+3);
+			printf("icm20602/icm42688 %10f, %10f, %10f, %10f, %10f, %10f,       %10f, %10f, %10f, %10f, %10f, %10f\r\n",
 					accf[0], accf[1], accf[2],
-					gyrof[0], gyrof[1], gyrof[2]);
-			acc[0] = (int16_t)((rsp[25] << 24) | (rsp[26] << 16) | (rsp[27] << 8) | (rsp[28] << 0));
-			acc[1] = (int16_t)((rsp[29] << 24) | (rsp[30] << 16) | (rsp[31] << 8) | (rsp[32] << 0));
-			acc[2] = (int16_t)((rsp[33] << 24) | (rsp[34] << 16) | (rsp[35] << 8) | (rsp[36] << 0));
-			gyro[0] = (int16_t)((rsp[37] << 24) | (rsp[38] << 16) | (rsp[39] << 8) | (rsp[40] << 0));
-			gyro[1] = (int16_t)((rsp[41] << 24) | (rsp[42] << 16) | (rsp[43] << 8) | (rsp[44] << 0));
-			gyro[2] = (int16_t)((rsp[45] << 24) | (rsp[46] << 16) | (rsp[47] << 8) | (rsp[48] << 0));
-			convert2float(acc, gyro, accf, gyrof);
-			printf("icm42688 %10f, %10f, %10f, %10f, %10f, %10f\r\n",
-					accf[0], accf[1], accf[2],
-					gyrof[0], gyrof[1], gyrof[2]);
+					gyrof[0], gyrof[1], gyrof[2],
+					accf[3], accf[4], accf[5],
+					gyrof[3], gyrof[4], gyrof[5]);
 #endif
 		} else {
 			printf("rcv errno %d\r\n", len);
